a195468252e123b10b588e8df3aefae2505e0516
Vanja <vv52@humboldt.edu>
2020-03-19 12:26:50 -0700

    took some ideas from TreeBuilder.cs and build a BuildList() function that returns a list of nodes with questions loaded from a text file.
    next I am going to write a BuildTree() function that utilizes a preorder traversal to build the actual tree itself.

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -2,10 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Node.cs;
-using File;
+using System;
+using System.Text;
 
 public class Tree
 {
     Node _root;
-    string[] questions = File.ReadAllLines(@"\gameInit.txt", Encoding.UTF8);
+
+    public List<Node> BuildList()
+    {
+    	List<Node> nodes = new List<Node>();
+    	string[] questions = System.IO.File.ReadAllLines("gameInit.txt");
+        foreach (var question in questions)
+        {
+            Node node = new Node();
+            node.question = question;
+
+            if(question.Length > 0 && question[0] == '*')
+            {
+                node.question = question.Substring(1);
+                node.isLeaf = true;
+            }
+            nodes.push_back(node);
+        }
+        return nodes;
+    }
+
+    public 
 }
